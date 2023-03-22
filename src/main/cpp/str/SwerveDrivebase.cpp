4fc2ffb5cdfc4d284ccecca84c0bee692a246cfb
Drew Williams <williams.r.drew@gmail.com>
2023-03-22 13:33:01 -0400

    added twist while rotating correction

--- src/main/cpp/str/SwerveDrivebase.cpp
+++ src/main/cpp/str/SwerveDrivebase.cpp
@@ -57,6 +57,7 @@ void str::SwerveDrivebase::AddVisionMeasurementToPoseEstimator(frc::Pose2d visio
   estimator.AddVisionMeasurement(visionMeasuredRobotPose, timeStampWhenPicWasTaken);
 }
 
+
 void str::SwerveDrivebase::Drive(
   units::meters_per_second_t xSpeed,
   units::meters_per_second_t ySpeed,
@@ -66,9 +67,15 @@ void str::SwerveDrivebase::Drive(
   bool voltageComp,
   bool rateLimit
 ) {
+
+  units::radian_t rot = rotSpeed * 20_ms;
+  frc::Pose2d robotPoseVel{xSpeed * 20_ms, ySpeed * 20_ms, frc::Rotation2d{rot}};
+  frc::Twist2d twistVel = frc::Pose2d{}.Log(robotPoseVel);
+  frc::ChassisSpeeds updatedChassisSpeeds{twistVel.dx / 20_ms, twistVel.dy / 20_ms, twistVel.dtheta / 20_ms};
+
   auto states = kinematics.ToSwerveModuleStates(
-    fieldRelative ? frc::ChassisSpeeds::FromFieldRelativeSpeeds(xSpeed, ySpeed, rotSpeed, imu.GetYaw() + imuDriverOffset) :
-                    frc::ChassisSpeeds(xSpeed, ySpeed, rotSpeed)
+    fieldRelative ? frc::ChassisSpeeds::FromFieldRelativeSpeeds(updatedChassisSpeeds.vx, updatedChassisSpeeds.vy, updatedChassisSpeeds.omega, imu.GetYaw() + imuDriverOffset) :
+                    frc::ChassisSpeeds(updatedChassisSpeeds.vx, updatedChassisSpeeds.vy, updatedChassisSpeeds.omega)
   );
 
   units::meters_per_second_t maxModuleSpeed = str::Units::ConvertAngularVelocityToLinearVelocity(
