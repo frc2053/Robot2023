280b06c6dd2873dc1e4d9784ea14617821cc8c48
Drew Williams <williams.r.drew@gmail.com>
2023-03-28 10:35:42 -0400

    added cone pole viz

--- src/main/include/subsystems/ArmSubsystem.h
+++ src/main/include/subsystems/ArmSubsystem.h
@@ -144,6 +144,26 @@ class ArmSubsystem : public frc2::SubsystemBase {
       frc::Color8Bit{frc::Color::kBlue}
     );
 
+  frc::MechanismRoot2d* mediumConeBase = armDisplay.GetRoot("MediumConeBase", 150 - 29.27500, 150);
+  frc::MechanismLigament2d* mediumConePole =
+  mediumConeBase->Append<frc::MechanismLigament2d>(
+    "MediumConePole",
+    34.092500, 
+    90_deg, 
+    6, 
+    frc::Color8Bit{frc::Color::kPurple}
+  );
+
+  frc::MechanismRoot2d* tallConeBase = armDisplay.GetRoot("TallConeBase", 150 - 46.161430, 150);
+  frc::MechanismLigament2d* tallConePole =
+  tallConeBase->Append<frc::MechanismLigament2d>(
+    "TallConePole",
+    46.062500, 
+    90_deg, 
+    6, 
+    frc::Color8Bit{frc::Color::kPurple}
+  );
+
   frc::MechanismRoot2d* tester = armDisplay.GetRoot("TESTER", 150, 150);
   frc::MechanismLigament2d* testerDot =
     tester->Append<frc::MechanismLigament2d>(
